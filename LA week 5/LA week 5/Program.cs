7da926f58fce238ed5387ab2dd3657aab6fe4138
Bryans91 <bjlcschr@avans.nl>
2016-03-30 14:20:58 +0200

    First

---
+++ LA week 5/LA week 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA_week_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vector in1 = new Vector(1,2,3);
+            Vector in2 = new Vector(5,4,3);
+
+            Console.WriteLine("Inproduct = " + inproduct(in1, in2));
+            Console.WriteLine("Uitproduct:");
+            uitproduct(in1, in2).printVector();
+
+
+            Console.ReadKey();
+
+        }
+
+        //Opdracht 1a
+        static double inproduct(Vector a, Vector b)
+        {
+            return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+        }
+
+        //opdract 1b
+        static Vector uitproduct(Vector a, Vector b)
+        {
+            double tempx = (a.y * b.z) - (b.y * a.z);
+            double tempy = (b.x * a.z) - (a.x * b.z);
+            double tempz = (a.x * b.y) - (b.x * a.y);
+
+            return new Vector(tempx, tempy, tempz);
+        }
+
+    }
+}
---
+++ LA week 5/LA week 5/Vector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA_week_5
+{
+    class Vector
+    {
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+
+
+
+        public void printVector(){
+            Console.WriteLine("(" + x + " )");
+            Console.WriteLine("(" + y + " )");
+            Console.WriteLine("(" + z + " )");
+        }
+    }
+}
