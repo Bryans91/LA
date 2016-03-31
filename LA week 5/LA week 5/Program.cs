2cc8af1fb0e154af7bd671ce68e62e54d4af19b5
Bryans91 <bjlcschr@avans.nl>
2016-03-31 22:09:33 +0200

    Week 5 af

--- LA week 5/LA week 5/Program.cs
+++ LA week 5/LA week 5/Program.cs
@@ -11,17 +11,25 @@ namespace LA_week_5
         static void Main(string[] args)
         {
 
-            Vector in1 = new Vector(1,2,3);
-            Vector in2 = new Vector(5,4,3);
+            Vector in1 = new Vector(6,6,0);
+            Vector in2 = new Vector(1,1,0);
 
             Console.WriteLine("Inproduct = " + inproduct(in1, in2));
+            
+            
+            
             Console.WriteLine("Uitproduct:");
             uitproduct(in1, in2).printVector();
-
+            opdracht3a(0, -0, -3, 12x);
 
             Console.ReadKey();
 
+           
+
         }
+
+
+
 
         //Opdracht 1a
         static double inproduct(Vector a, Vector b)
@@ -37,6 +45,39 @@ namespace LA_week_5
             double tempz = (a.x * b.y) - (b.x * a.y);
 
             return new Vector(tempx, tempy, tempz);
+        }
+
+
+        //opdracht 3a
+        static void opdracht3a(double p,double q,double r, double s){
+
+            Vector steun = new Vector(0,0,0);
+            Vector a = new Vector(0, 0, 0);
+            Vector b = new Vector(0, 0, 0);
+
+            if (p != 0)
+            {
+                steun = new Vector(s / p, 0, 0);
+                a = new Vector(-q, p, 0);
+                b = new Vector(-r, 0, p);
+            }
+            else if (p == 0 && q != 0)
+            {
+                steun = new Vector(0, s/q, 0);
+                a = new Vector(q, 0, 0);
+                b = new Vector(0, -r ,q);
+            }
+            else if (p == 0 && q == 0 && r != 0)
+            {
+                steun = new Vector(0, 0, s/r);
+                a = new Vector(1, 0, 0);
+                b = new Vector(0, 1, 0);
+            }
+            
+            Console.WriteLine("("+steun.x+")    ("+a.x+")    ("+b.x+")");
+            Console.WriteLine("(" + steun.y + ") + a(" + a.y + ") + u(" + b.y + ")");
+            Console.WriteLine("(" + steun.z + ")    (" + a.z + ")    (" + b.z + ")");
+            
         }
 
     }
