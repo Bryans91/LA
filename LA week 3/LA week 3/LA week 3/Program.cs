8a4d71985aef0662c831bf562ef155e18fdfe136
Bryans91 <bjlcschr@avans.nl>
2016-04-09 17:24:43 +0200

    Week 3 Translatie matrix en Scale matrix berekeningen toegevoegd

--- LA week 3/LA week 3/LA week 3/Program.cs
+++ LA week 3/LA week 3/LA week 3/Program.cs
@@ -20,7 +20,7 @@ namespace LA_week_3
             matrix1.setValue(0, 2, 0, 7);
             matrix1.setValue(1, 2, 0, 8);
             matrix1.setValue(2, 2, 0, 9);
-
+ 
             matrix1.setValue(0, 0, 1, 9);
             matrix1.setValue(1, 0, 1, 8);
             matrix1.setValue(2, 0, 1, 7);
@@ -31,12 +31,63 @@ namespace LA_week_3
             matrix1.setValue(1, 2, 1, 2);
             matrix1.setValue(2, 2, 1, 1);
 
-            Console.WriteLine("3d matrix print test");
 
+            Console.WriteLine("3d matrix print test");
             matrix1.print();
 
+            Console.WriteLine("Scale matrix test");
+            scale(6.2, 3.8, 1.3).print();
+
+            Console.WriteLine("Translate matrix test");
+            translate(12, 23, -8).print();
 
             Console.ReadKey();
         }
+
+
+        public static Matrix scale(double x,double y,double z){
+
+            Matrix result = new Matrix(3, 3, 1);
+            result.setValue(0, 0, 0, x);
+            result.setValue(1, 0, 0, 0);
+            result.setValue(2, 0, 0, 0);
+            result.setValue(0, 1, 0, 0);
+            result.setValue(1, 1, 0, y);
+            result.setValue(2, 1, 0, 0);
+            result.setValue(0, 2, 0, 0);
+            result.setValue(1, 2, 0, 0);
+            result.setValue(2, 2, 0, z);
+
+            return result;
+        }
+
+        public static Matrix translate(double x, double y, double z){
+            Matrix result = new Matrix(4, 4, 1);
+            result.setValue(0, 0, 0, 1);
+            result.setValue(1, 0, 0, 0);
+            result.setValue(2, 0, 0, 0);
+            result.setValue(3, 0, 0, x);
+
+            result.setValue(0, 1, 0, 0);
+            result.setValue(1, 1, 0, 1);
+            result.setValue(2, 1, 0, 0);
+            result.setValue(3, 1, 0, y);
+
+            result.setValue(0, 2, 0, 0);
+            result.setValue(1, 2, 0, 0);
+            result.setValue(2, 2, 0, 1);
+            result.setValue(3, 2, 0, z);
+
+            result.setValue(0, 3, 0, 0);
+            result.setValue(1, 3, 0, 0);
+            result.setValue(2, 3, 0, 0);
+            result.setValue(3, 3, 0, 1);
+
+
+            return result;
+        }
+
+
+
     }
 }
